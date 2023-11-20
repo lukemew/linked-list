5ba7deb9827742d475fb114d0e5d5679aae4bb5b
lukemew <123739531+lukemew@users.noreply.github.com>
2023-11-20 08:52:23 -0300

    achieving the goal

--- basic_linked-list.js
+++ basic_linked-list.js
@@ -3,9 +3,40 @@ function linkedList(){
     let head = null
     let length = 0
 
-    return{
-        length: () => length
+    const newNode = (value) => {
+        return{
+            value,
+            next: null
+        }
     }
+
+    const add = (value) => {
+        if(!head){
+            head = newNode(value)
+            length++
+            return head
+        }
+        let node = head
+        while(node.next){
+            node = node.next
+        }
+        node.next = newNode(value)
+        length++
+        return node.next
+    }
+
+    return{
+        length: () => length,
+        add: (value) => add(value),
+        print: () => console.log(head)
+    }
+     
 }
+
 const list = linkedList()
 console.log(list.length())
+list.add(7)
+list.add(8)
+list.add(9)
+console.log(list.length())
+list.print()
