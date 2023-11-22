cf34ab92c3494a19c1721708a6373c3aa269febb
lukemew <123739531+lukemew@users.noreply.github.com>
2023-11-22 09:16:15 -0300

    Adding some search functions & adding comments in the code

--- basic_linked-list.js
+++ basic_linked-list.js
@@ -1,8 +1,9 @@
 function linkedList(){
-
+    // a variável head começa sendo apontada para null
     let head = null
     let length = 0
 
+    // função para criar um novo nó
     const newNode = (value) => {
         return{
             value,
@@ -10,33 +11,76 @@ function linkedList(){
         }
     }
 
+    // adiciona os nós na lista
     const add = (value) => {
+        
+        // caso head não exista, é criado um novo valor fazendo parte de head
         if(!head){
             head = newNode(value)
             length++
             return head
         }
+        // a variável node recebe o valor de head, então percorre a lista até chegar no último elemento
         let node = head
         while(node.next){
             node = node.next
         }
+        // ao chegar ao final, o valor de node.next será o item que vem depois do último item da lista, que então é criado
         node.next = newNode(value)
         length++
         return node.next
     }
 
+    const getByIndex = (index) => {
+        if(length === 0 || index >= length){
+            return null
+        }
+        let node = head
+        let count = 0
+        while(count < index  && node.next){
+            node = node.next
+            count++
+        }
+        return node
+    }
+
+    const getByValue = (value) => {
+        if(length === 0){
+            return null
+        }
+        let node = head
+        
+        if(node.value === value){
+            return node
+        } 
+        while(node.next){
+            node = node.next
+            if(node.value === value){
+                return node
+            } 
+        }
+        return null
+    }
+
     return{
+        // ============== FUNÇÕES ================
+
+        // retorna o tamanho atual da lista
         length: () => length,
+        // serve para adicionar valores
         add: (value) => add(value),
-        print: () => console.log(head)
+        // ajuda a mostrar a lista de uma maneira mais compreensivel
+        print: () => console.log(head),
+        // procura algum elemento da lista através do índice
+        getByIndex: (index) => getByIndex(index),
+        // procura algum elemento da lista através do valor
+        getByValue: (value) => getByValue(value)
     }
      
 }
 
 const list = linkedList()
-console.log(list.length())
 list.add(7)
 list.add(8)
 list.add(9)
-console.log(list.length())
-list.print()
+console.log(list.getByValue(2))
